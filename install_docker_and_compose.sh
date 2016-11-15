9beb0150b0618662bd11a6617df3878621e86b78
chatelao <olivier.chatelain@gmail.com>
2016-11-15 20:07:47 +0100

    Chg: Add nearly "ready-to-run" config.

---
+++ install_docker_and_compose.sh
@@ -0,0 +1,5 @@
+sudo curl -sSL https://get.docker.com/ | sh
+sudo -i
+curl -L "https://github.com/docker/compose/releases/download/1.8.1/docker-compose-$(uname -s)-$(uname -m)" > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose
